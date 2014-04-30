33f2d45c530e3d05efb4e578262d6fcf5bb9099b
Juan Matthys Uys <opyate@gmail.com>
2014-04-30 15:55:57 +0100

    initial grid

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,18 +8,28 @@ angular.module('elasticsearchAngularjsPoweredApp')
     var client = ejs.Request()
         .indices('my_index');
     $scope.resultsArr = [];
+
+    $scope.per_page = 25;
+    $scope.page = 0;
  
-    $scope.search = function() {
+    $scope.show_more = function () {
+        $scope.page += 1;
+        $scope.search($scope.page*$scope.per_page);
+    };
+ 
+    $scope.search = function(offset) {
         $scope.resultsArr = [];
         var results = client
             .query(query.query($scope.queryTerm || '*'))
+            .from(offset)
+            .size($scope.per_page)
             .doSearch();
         $q.when(results).then(function(result) {
           $scope.resultsArr.push(result);        
         });
     };
 
-    $scope.search();
+    $scope.search($scope.page);
 
   }]);
 
