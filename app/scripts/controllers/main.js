50e6b56f5bd08c63f7494f0479fcb0bad4bcefff
Juan Matthys Uys <opyate@gmail.com>
2014-04-30 09:09:48 +0100

    displaying a result

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('elasticsearchAngularjsPoweredApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', 'ejsResource', '$q', function ($scope, ejsResource, $q) {
+    
+    var ejs = ejsResource('https://opyate.west-eu.azr.facetflow.io');
+    var query = ejs.QueryStringQuery().defaultField('message');
+    var client = ejs.Request()
+        .indices('my_index');
+    $scope.resultsArr = [];
+ 
+    $scope.search = function() {
+        $scope.resultsArr = [];
+        var results = client
+            .query(query.query($scope.queryTerm || '*'))
+            .fields(['message', 'user', 'post_date'])
+            .doSearch();
+        $q.when(results).then(function(result) {
+          console.log(result);
+          $scope.resultsArr.push(result);        
+        });
+    };
+
+  }]);
+
