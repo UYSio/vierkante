3592cb86b134eb7e2da62b671626f6439b478f15
Juan Matthys Uys <opyate@gmail.com>
2014-04-30 15:12:19 +0100

    dynamic templates

---
+++ app/scripts/directives/draw.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// from https://coderwall.com/p/mgtrkg
+
+angular.module('elasticsearchAngularjsPoweredApp')
+  .directive('draw', ['$compile', '$http', '$templateCache', function ($compile, $http, $templateCache) {
+
+  var getTemplate = function(contentType) {
+    var templateLoader,
+    baseUrl = 'views/',
+    templateMap = {
+      post: '_post.html',
+      posts: '_post.html',
+      photo: '_photo.html',
+      video: '_video.html',
+      tweet: '_tweet.html',
+      link: '_link.html',
+      audio: '_audio.html'
+    };
+    var templateUrl = baseUrl + templateMap[contentType];
+    templateLoader = $http.get(templateUrl, {cache: $templateCache});
+    return templateLoader;
+  };
+
+  var linker = function(scope, element, attrs) {
+
+    var loader = getTemplate(scope.thing._type);
+
+    var promise = loader.success(function(html) {
+        element.html(html);
+    }).then(function (response) {
+        element.replaceWith($compile(element.html())(scope));
+    });
+  };
+
+  return {
+      restrict: 'E',
+      scope: {
+        thing: '='
+      },
+      link: linker
+    };
+  }]);
