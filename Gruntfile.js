d0a88a53ae8708122f41316001ceaca7d39c6465
Juan Matthys Uys <opyate@gmail.com>
2014-05-27 13:24:35 +0100

    heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,6 +292,18 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
+        }, {
+          expand: true,
+          dest: '<%= yeoman.dist %>',
+          cwd: 'heroku',
+          src: '*',
+          rename: function (dest, src) {
+            var path = require('path');
+            if (src === 'distpackage.json') {
+              return path.join(dest, 'package.json');
+            }
+            return path.join(dest, src);
+          }
         }]
       },
       styles: {
